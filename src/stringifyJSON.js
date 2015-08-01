8c612fbb616198c108f59a67f2dd49481953d4be
lainjiang <lain.lai.jiang@gmail.com>
2015-07-31 21:42:03 -0500

    Wrote pseudo-code

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,46 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	var res = [];
+
+	if input is primitive data type {
+		return input;
+	}
+
+	if input is function, return null;
+
+	if input is array,
+
+		res.push('[');
+
+		for (var i=0; i<array.length; i++)
+			
+			res.push(recursive(array[i]));
+			if (i<array.length-1){
+				res.push(',');
+			}
+
+		});
+
+		res.push(']');
+
+	if input is non-array obj,
+
+		res.push('{');
+		var count = 0;
+		var length = length of obj;
+
+		for (var prop in input){
+			count++;
+			res.push(prop+';');
+			res.push(recursive(input[obj]));
+			if (count<length){
+				res.push('');
+			}
+		}
+
+		res.push('}');
+
+
+	return res.join('');
 };
